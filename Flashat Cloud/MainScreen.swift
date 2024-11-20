83274c76d2abd8a2859fe5a9b25c43d62233176f
shagarios <shagar.ios.dev@gmail.com>
2024-11-20 23:43:48 +0530

    initial commit

---
+++ Flashat Cloud/MainScreen.swift
@@ -0,0 +1,18 @@
+//
+//  MainScreen.swift
+//  Flashat Cloud
+//
+//  Created by shagar on 20/11/24.
+//
+
+import SwiftUI
+
+struct MainScreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainScreen()
+}
